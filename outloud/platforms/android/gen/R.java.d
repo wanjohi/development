8b53a40488888a0589c66a1bf7f64254ded4c876
wanjohi <wanjohi@gmail.com>
2013-10-24 22:35:59 -0700

    first upload

---
+++ outloud/platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+A:\development\outloud\platforms\android\gen\com\wain\outloud\R.java \
+ : A:\development\outloud\platforms\android\res\drawable\icon.png \
+A:\development\outloud\platforms\android\res\drawable-hdpi\icon.png \
+A:\development\outloud\platforms\android\res\drawable-ldpi\icon.png \
+A:\development\outloud\platforms\android\res\drawable-mdpi\icon.png \
+A:\development\outloud\platforms\android\res\drawable-xhdpi\icon.png \
+A:\development\outloud\platforms\android\res\values\strings.xml \
+A:\development\outloud\platforms\android\res\xml\config.xml \
+A:\development\outloud\platforms\android\bin\AndroidManifest.xml \
---
+++ outloud/platforms/android/gen/com/wain/outloud/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.wain.outloud;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
